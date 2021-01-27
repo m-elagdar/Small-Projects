871dc7118b47ec6a79ea04d674c04942cc02806e
Mahmoud <ma7mouda7mad@gmail.com>
2021-01-28 00:39:28 +0200

    fix lan forwarding in A-WireGuard-CLI

--- A-WireGuard-CLI/init_server.sh
+++ A-WireGuard-CLI/init_server.sh
@@ -4,7 +4,7 @@ set -e
 
 net=1
 : ${lan_name:=$(ip route | grep default | grep -oP "(?<=dev )\S+")}
-echo "Setting interface for network forwarding to $lan_name"
+echo "Setting interface for network forwarding to $lan_name. Set \$lan_name to change it"
 server_wireguard_address=172.$net.1.1
 
 : ${conf_file:=server$net.conf}
@@ -24,10 +24,15 @@ Address = $server_wireguard_address
 PrivateKey = $(cat "$keys_dir"/private)
 ListenPort = 51820
 
+EOF
+read -p "Forward your local network so your peers can connect to devices in your lan (Y,n): " ok
+if [[ $ok =~ ^([yY](es|a|up|eah)*|)$ ]]; then sudo tee -a /etc/wireguard/$conf_file << EOF > /dev/null
 PostUp   = iptables -A FORWARD -i %i -j ACCEPT; iptables -A FORWARD -o %i -j ACCEPT; iptables -t nat -A POSTROUTING -o $lan_name -j MASQUERADE
 PostDown = iptables -D FORWARD -i %i -j ACCEPT; iptables -D FORWARD -o %i -j ACCEPT; iptables -t nat -D POSTROUTING -o $lan_name -j MASQUERADE
 
 EOF
+printf "\nYour should now add/uncomment \"net.ipv4.ip_forward = 1\" in /etc/sysctl.d/99-sysctl.conf or /etc/sysctl.conf\nThen run \"sudo sysctl -p\" and make sure the line is printed"
+fi
 else echo "Not replacing /etc/wireguard/$conf_file. You may delete it and rerun the script"; fi
 
 echo "Configuration ready. You may share your public key with your peers:"
